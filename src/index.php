370cddb80707649d87657a0827a5d795b2b27b8b
IeltxuSV <ieltxusv@ni.eus>
2024-01-26 18:07:59 +0100

    Dividido los enlaces en bloques

--- src/index.php
+++ src/index.php
@@ -1,19 +1,35 @@
 <!DOCTYPE html>
 <html lang="eus">
+
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Erraldoiak</title>
+
 </head>
+
 <body>
     <h1>Erraldoiak</h1>
 
     <div class="estekak">
-        <a href="#">Dantzariak</a>
-        <a href="#">Dantzak</a>
-        <a href="#">Ekitaldiak</a>
+        <div class="blokea">
+            <a href="#">Dantzariak</a>
+            <!-- Icon -->
+        </div>
+        <div class="blokea">
+            <a href="#">Dantzak</a>
+            <!-- Icon -->
+        </div>
+        <div class="blokea">
+            <a href="#">Ekitaldiak</a>
+            <!-- Icon -->
+        </div>
+
         <a href="#">Deslogeatu</a>
+
     </div>
 </body>
+
 </html>
