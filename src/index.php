a21bfaa294ee5d4611f5979f207ee8bf6845b0ee
IeltxuSV <ieltxusv@ni.eus>
2024-01-26 17:32:28 +0100

    Creando la estructura
    Creando la extructura y el index.php

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="eus">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erraldoiak</title>
+</head>
+<body>
+    <h1>Erraldoiak</h1>
+</body>
+</html>
