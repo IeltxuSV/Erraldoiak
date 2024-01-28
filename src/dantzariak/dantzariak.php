9a093e3c6dc4ad8935a7ea8e47f1f001ed209a4e
IeltxuSV <ieltxusv@ni.eus>
2024-01-28 19:06:59 +0100

    Se a creado dantzariak y su archivo de estilo
    Se a creado el archivo dantzariak.php y dantzariak.css para crear el estilo

---
+++ src/dantzariak/dantzariak.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="eus">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dantzariak</title>
+
+    <link rel="stylesheet" href="../styles/dantzariak.css">
+</head>
+<body>
+    <h2>Dantzariak</h2>
+</body>
+</html>
