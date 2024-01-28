8df0cd7a5800cd133eb79a5f6d5ed061a6773864
IeltxuSV <ieltxusv@ni.eus>
2024-01-28 14:36:41 +0100

    Creaccion de nuevas tablas
    Creaccion de las ultimas tablas que quedaban
    Faltarioan los Foreging Keys

--- DB/datuBasea.sql
+++ DB/datuBasea.sql
@@ -21,3 +21,39 @@ CREATE TABLE IF NOT EXISTS Ekitaldiak (
     Ibilbide VARCHAR (300),
     PRIMARY KEY (ID_ekitaldia)
 );
+
+CREATE TABLE IF NOT EXISTS  Dantzariak (
+    ID_dantzari INT AUTO_INCREMENT,
+    Izena VARCHAR(40) NOT NULL,
+    Erra_Gustokoa INT,
+    Dant_gustokoa INT,
+    PRIMARY KEY (ID_dantzari)
+);
+
+CREATE TABLE IF NOT EXISTS DakitenDantza (
+    Dantzari INT,
+    Dantza INT,
+    Generoa VARCHAR (20),
+    PRIMARY KEY (Dantzari, dantza)
+);
+
+CREATE TABLE IF NOT EXISTS EkitaldianEramaten (
+    Ekitaldia INT,
+    Dantzari INT,
+    Erraldoia INT,
+    PRIMARY KEY (Ekitaldia, Dantzari, Erraldoia)
+);
+
+-- Se hace esto para una base que hay 6 gigantes abria que modificarlo depende de cuantos abria
+CREATE TABLE IF NOT EXISTS EkitaldiaDantza (
+    ID_ekitaldiaDantza INT AUTO_INCREMENT,
+    Ekitaldia INT NOT NULL,
+    Dantza INT NOT NULL,
+    Mutila1 INT,
+    Mutila2 INT,
+    Mutila3 INT,
+    Neska1 INT,
+    Neska2 INT,
+    Neska3 INT,
+    PRIMARY KEY (ID_ekitaldiaDantza)
+);
