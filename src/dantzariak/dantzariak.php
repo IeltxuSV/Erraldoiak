78a04673f7db04f817ab04e937c88fe471c3c689
IeltxuSV <ieltxusv@ni.eus>
2024-01-29 18:14:08 +0100

    Reajustado los estilos y archivos
    Se a creado un archivo general para centralizar todos los estilos globales. Y se a puesto el enlace a cada uno para que lo pille. Se a creado el diseño del boton de para atras

--- src/dantzariak/dantzariak.php
+++ src/dantzariak/dantzariak.php
@@ -6,9 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dantzariak</title>
 
+    <link rel="stylesheet" href="../styles/styles.css">
     <link rel="stylesheet" href="../styles/dantzariak.css">
+    <script src="https://kit.fontawesome.com/dacbd6cb00.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <h2>Dantzariak</h2>
+
+    <div class="dantzariak">
+
+    </div>
+
+    <div class="atzera">
+        <a href="../index.php"><i class="fa-solid fa-circle-chevron-left"></i></a>
+    </div>
 </body>
 </html>
--- src/index.php
+++ src/index.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Erraldoiak</title>
 
+    <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="styles/index.css">
     <script src="https://kit.fontawesome.com/dacbd6cb00.js" crossorigin="anonymous"></script>
 </head>
