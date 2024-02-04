91dd4238dc7e3df8315ac5977112af81fa8ef3da
IeltxuSV <ieltxusv@ni.eus>
2024-02-04 16:16:52 +0100

    Se a añadido el boton de añadir
    Se a añadido el boton de añadir que todavia no tiene funcionamiento y se a añadido un funcion para mostrar a los dantzaris

--- src/dantzariak/dantzariak.php
+++ src/dantzariak/dantzariak.php
@@ -1,5 +1,12 @@
+<?php
+include("../conection.php");
+$conn = conectar();
+include("funtzioak.php")
+    ?>
+
 <!DOCTYPE html>
 <html lang="eus">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,15 +17,25 @@
     <link rel="stylesheet" href="../styles/dantzariak.css">
     <script src="https://kit.fontawesome.com/dacbd6cb00.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <h2>Dantzariak</h2>
 
     <div class="dantzariak">
+        <?php
+            mostrardantzaris($conn)
+        ?>
+    </div>
+    <div class="botoiak">
+        <div class="gehitu">
+            <a href="#" class="botoia"><i class="fa-solid fa-circle-plus"></i></a>
+        </div>
 
+        <div class="atzera">
+            <a href="../index.php" class="botoia"><i class="fa-solid fa-circle-chevron-left"></i></a>
+        </div>
     </div>
 
-    <div class="atzera">
-        <a href="../index.php"><i class="fa-solid fa-circle-chevron-left"></i></a>
-    </div>
 </body>
+
 </html>
