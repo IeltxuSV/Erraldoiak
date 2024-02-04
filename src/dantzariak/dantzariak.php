f487fa1ad15f175accb24d33c276ef222df8cf7e
IeltxuSV <ieltxusv@ni.eus>
2024-02-04 15:31:43 +0100

    Estilos
    Se a cambiado el nombre de los archivos genrerales

--- src/dantzariak/dantzariak.php
+++ src/dantzariak/dantzariak.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dantzariak</title>
 
-    <link rel="stylesheet" href="../styles/styles.css">
+    <link rel="stylesheet" href="../styles/main.css">
     <link rel="stylesheet" href="../styles/dantzariak.css">
     <script src="https://kit.fontawesome.com/dacbd6cb00.js" crossorigin="anonymous"></script>
 </head>
